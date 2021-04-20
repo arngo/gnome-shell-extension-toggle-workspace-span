98be70ccf3b118ccc113780ddf6489dc8327b9c5
arngo <27396817+arngo@users.noreply.github.com>
2021-04-20 03:31:04 -0400

    update for gnome 40 and remove compatibility <3.36

--- toggle-workspace-span@arngo.github.com/extension.js
+++ toggle-workspace-span@arngo.github.com/extension.js
@@ -11,9 +11,6 @@ const PanelMenu = imports.ui.panelMenu;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
-const Config = imports.misc.config;
-const SHELL_MINOR = parseInt(Config.PACKAGE_VERSION.split('.')[1]);
-
 let mutterSettings = null;
 try {
     mutterSettings = ExtensionUtils.getSettings('org.gnome.mutter');
@@ -21,48 +18,43 @@ try {
     logError(e, 'Failed to load Mutter settings');
 }
 
-var ToggleButton = class ToggleButton extends PanelMenu.Button {
-    getIcon(state) {
-        if (state) {
-            return Gio.icon_new_for_string(Me.path + '/icons/workspace-span-off-symbolic.svg');
-        } else {
-            return Gio.icon_new_for_string(Me.path + '/icons/workspace-span-on-symbolic.svg');
+var ToggleButton = GObject.registerClass(
+    {GTypeName: 'ToggleButton'},
+    class ToggleButton extends PanelMenu.Button {
+        getIcon(state) {
+            if (state) {
+                return Gio.icon_new_for_string(Me.path + '/icons/workspace-span-off-symbolic.svg');
+            } else {
+                return Gio.icon_new_for_string(Me.path + '/icons/workspace-span-on-symbolic.svg');
+            }
+        }
+
+        _init() {
+            super._init(0.0, `${Me.metadata.name} Indicator`, false);
+            this.icon = new St.Icon({style_class: "system-status-icon"});
+            this.updateIcon();
+            this.add_child(this.icon);
+            this._onPressEventId = this.connect('button-press-event', this.pressAction.bind(this));
+            this._onSettingChangedId = mutterSettings.connect('changed::workspaces-only-on-primary', this.updateIcon.bind(this));
+        }
+
+        updateIcon() {
+            this.icon.gicon = this.getIcon(mutterSettings.get_boolean('workspaces-only-on-primary'));
+        }
+
+        pressAction() {
+            let current = mutterSettings.get_boolean('workspaces-only-on-primary');
+            mutterSettings.set_boolean('workspaces-only-on-primary', !current);
+            Main.overview.hide();
+        }
+
+        destroy() {
+            this.disconnect(this._onPressEventId);
+            this.disconnect(this._onSettingChangedId);
+            super.destroy();
         }
     }
-
-    _init() {
-        super._init(0.0, `${Me.metadata.name} Indicator`, false);
-        this.icon = new St.Icon({style_class: "system-status-icon"});
-        this.updateIcon();
-        this.add_child(this.icon);
-        this._onPressEventId = this.connect('button-press-event', this.pressAction.bind(this));
-        this._onSettingChangedId = mutterSettings.connect('changed::workspaces-only-on-primary', this.updateIcon.bind(this));
-    }
-
-    updateIcon() {
-        this.icon.gicon = this.getIcon(mutterSettings.get_boolean('workspaces-only-on-primary'));
-    }
-
-    pressAction() {
-        let current = mutterSettings.get_boolean('workspaces-only-on-primary');
-        mutterSettings.set_boolean('workspaces-only-on-primary', !current);
-        Main.overview.hide();
-    }
-
-    destroy() {
-        this.disconnect(this._onPressEventId);
-        this.disconnect(this._onSettingChangedId);
-        super.destroy();
-    }
-}
-
-// Compatibility with gnome-shell >= 3.32
-if (SHELL_MINOR > 30) {
-    ToggleButton = GObject.registerClass(
-        {GTypeName: 'ToggleButton'},
-        ToggleButton
-    );
-}
+);
 
 var button = null;
 
